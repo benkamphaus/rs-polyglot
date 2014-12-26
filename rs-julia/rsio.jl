f090480dfb471b6fbf3a82ef3e365c7758a20590
Ben Kamphaus <ben.kamphaus@gmail.com>
2014-12-26 12:22:11 -0700

    Better to just use Julia's overloaded read

--- rs-julia/rsio.jl
+++ rs-julia/rsio.jl
@@ -1,23 +1,11 @@
-doc = 
-"""
-Reads a flat binary file into an array. Dimensions and data type are
-parameters.
-"""
 function readbinary(f::String, dims::Array{Int64, 1}, dt::DataType)
-
-    # initialize array and element count
-    n = reduce(*, dims)
-    arr = Array(dt, n)
-
-    # open file and read in values of appropriate data type
-    of = open(f)
-    for i in 1:n
-        arr[i] = read(of, dt)
-    end
-
-    # return reshape of flat array we read in
+    arr = Array(dt, dims...)
+    of = open(f, "r")
+    read!(of, arr)
     return arr
-#    return reshape(arr, dims...)
 end
 
 @time readbinary("data/nireland.dat", [472, 682, 128], Int16)
+
+fp = open("data/nireland.dat", "r")
+@time read(fp, Int16, (472, 682, 128))
